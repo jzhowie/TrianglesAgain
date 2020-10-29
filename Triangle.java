0ef83cbcd537f96a253cec8285cd9f32058d3c69
jzhowie <jzhou20@stuy.edu>
2020-10-29 14:15:50 -0400

    Created toString method

--- Triangle.java
+++ Triangle.java
@@ -12,4 +12,9 @@ public Triangle (double x1, double x2, double x3, double y1, double y2, double y
 	v2 = new Point (x2, y2);
 	v3 = new Point (x3, y3);
 }
+
+public String toString() {
+	String tri = "v1(" + v1.getX() + ", " + v1.getY() + ") " + "v2(" + v2.getX() + ", " + v2.getY() + ") " + "v3(" + v3.getX() + ", " + v3.getY();
+	return tri + ")";
+}
 }
