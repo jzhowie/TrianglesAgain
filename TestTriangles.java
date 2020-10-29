48f49f5837dd9d40595246662efb586d756b745c
jzhowie <jzhou20@stuy.edu>
2020-10-29 13:50:00 -0400

    Added test cases for Point Class

--- TestTriangles.java
+++ TestTriangles.java
@@ -1,5 +1,13 @@
 public class TestTriangles {
 public static void main(String[] args) {
 
+	Point P1 = new Point (5, 7);
+	Point P2 = new Point (1, 4);
+
+	System.out.println("Point clone testing:");
+	Point P3 = new Point (P2);
+	//(1, 4)
+	System.out.println(P3.getX());
+	System.out.println(P3.getY());
 }
 }
